fb302ecc8b51b493d72d71088ae6406beea00a8f
subzone05-pixel <subzone05@gmail.com>
2026-09-15 02:10:48 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,9 +5,10 @@ mkdir -p /app/live
 # Nginx স্টার্ট
 nginx
 
-# পিওর ডিরেক্ট কপি মোড (জিরো সিপিইউ লোড, রিয়েল-টাইম স্পিড)
+# রিয়েল-টাইম লাইভ মোড উইথ জিরো বাফারিং
 ffmpeg -re \
+  -fflags nobuffer -flags low_delay \
   -f concat -safe 0 -protocol_whitelist file,http,https,tcp,tls -stream_loop -1 -i /app/playlist.txt \
   -c copy \
-  -f hls -hls_time 3 -hls_list_size 10 -hls_flags delete_segments \
+  -f hls -hls_time 2 -hls_list_size 3 -hls_flags delete_segments+append_list \
   /app/live/stream.m3u8
