47a973ed76bca6c7a842133ec1629e6f6896bf4f
subzone05-pixel <subzone05@gmail.com>
2026-09-15 23:46:47 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,13 +2,17 @@
 
 mkdir -p /app/live
 
-# Nginx স্টার্ট
+# Nginx চালু করা
 nginx
 
-# রিয়েল-টাইম লাইভ মোড উইথ জিরো বাফারিং
+# লোগো ওভারলেসহ HLS স্ট্রিম
 ffmpeg -re \
   -fflags nobuffer -flags low_delay \
   -f concat -safe 0 -protocol_whitelist file,http,https,tcp,tls -stream_loop -1 -i /app/playlist.txt \
-  -c copy \
+  -i /app/logo.jpg \
+  -filter_complex "[1:v]scale=200:-1[logo];[0:v][logo]overlay=W-w-20:20[v_out]" \
+  -map "[v_out]" -map 0:a? \
+  -c:v libx264 -preset ultrafast -tune zerolatency \
+  -c:a copy \
   -f hls -hls_time 2 -hls_list_size 3 -hls_flags delete_segments+append_list \
   /app/live/stream.m3u8
